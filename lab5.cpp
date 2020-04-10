a34a0ad033ca0e30d3feedcea29a7244f6bda7d5
Phlana <joshua.chang422@gmail.com>
2020-04-10 17:28:51 -0600

    lab5.cpp should be done now
    space indenting changed to tab indenting is why a section of code looks like it has been edited

--- lab5.cpp
+++ lab5.cpp
@@ -15,7 +15,6 @@
 
 #define MAX_COUNTER_BUFFER_LENGTH 100
 
-
 extern "C" {
 void UserMain(void * pd);
 void DisplayLameCounter( int sock, PCSTR url );
@@ -33,44 +32,103 @@ FormData myData;
 
 
 void UserMain(void * pd) {
-    InitializeStack();
-    OSChangePrio(MAIN_PRIO);
-    EnableAutoUpdate();
-    StartHTTP();
-    EnableTaskMonitor();
+	InitializeStack();
+	OSChangePrio(MAIN_PRIO);
+	EnableAutoUpdate();
+	StartHTTP();
+	EnableTaskMonitor();
 
-    #ifndef _DEBUG
-    EnableSmartTraps();
-    #endif
+	#ifndef _DEBUG
+	EnableSmartTraps();
+	#endif
 
-    #ifdef _DEBUG
-    InitializeNetworkGDB_and_Wait();
-    #endif
+	#ifdef _DEBUG
+	InitializeNetworkGDB_and_Wait();
+	#endif
 
-    myStepper.Init(STEPPER_MODE_FULL_STEP);
-    myKeypad.Init(KEYPAD_POLL_MODE);  // Interrupts would work too
-    myData.Init();
-    myLCD.Init();
-    myLCD.Clear();
-    myAD.Init();
+	myStepper.Init(STEPPER_MODE_FULL_STEP);
+	myKeypad.Init(KEYPAD_POLL_MODE);  // Interrupts would work too
+	myData.Init();
+	myLCD.Init();
+	myLCD.Clear();
+	myAD.Init();
 	RegisterPost();
 
 	iprintf("Application started\n");
-    while (1) {
-        OSTimeDly(TICKS_PER_SECOND);
-    }
+	while (1) {
+		OSTimeDly(TICKS_PER_SECOND);
+	}
 }
 
-void DisplayLameCounter( int sock, PCSTR url )
-{
+void DisplayLameCounter(int sock, PCSTR url) {
 	static int form_counter = 0;
 	char buffer[MAX_COUNTER_BUFFER_LENGTH+1];
 
-	if((sock > 0) && (url != NULL)) {
+	if ((sock > 0) && (url != NULL)) {
 		iprintf(url);
-		snprintf(buffer,MAX_COUNTER_BUFFER_LENGTH, "<H1>The page has been reloaded %d times. </H1>", form_counter );
+		snprintf(buffer, MAX_COUNTER_BUFFER_LENGTH, "<H1>The page has been reloaded %d times. </H1>", form_counter);
 		form_counter++;
-		writestring(sock,(const char *) buffer);
+		writestring(sock, (const char *)buffer);
+	}
+}
 
+void motorHandler(int sock, PCSTR url) {
+	char buffer[MAX_COUNTER_BUFFER_LENGTH+1];
+
+	if ((sock > 0) && (url != NULL)) {
+		iprintf(url);
+		myData.Lock();
+		char *motorError = myData.GetMotorErrorMessage();
+		myData.Unlock();
+
+		snprintf(buffer, MAX_COUNTER_BUFFER_LENGTH, motorError);
+		writestring(sock, (const char *)buffer);
+	}
+}
+
+void LCDHandler(int sock, PCSTR url) {
+	char buffer[MAX_COUNTER_BUFFER_LENGTH+1];
+
+	if ((sock > 0) && (url != NULL)) {
+		iprintf(url);
+		myData.Lock();
+		int LCDError = myData.GetLCDErrorMessage();
+		myData.Unlock();
+
+		snprintf(buffer, MAX_COUNTER_BUFFER_LENGTH, LCDError);
+		writestring(sock, (const char *)buffer);
+	}
+}
+
+void keypadHandler(int sock, PCSTR url) {
+	char buffer[MAX_COUNTER_BUFFER_LENGTH+1];
+
+	if ((sock > 0) && (url != NULL)) {
+		iprintf(url);
+		myData.Lock();
+		int keypadKey = myData.GetKeypadKey();
+		myData.Unlock();
+
+		if (keypadKey < 16) {
+			snprintf(buffer, MAX_COUNTER_BUFFER_LENGTH, "<img src=\"Keypad%d.bmp\">", keypadKey);
+		}
+		else {
+			snprintf(buffer, MAX_COUNTER_BUFFER_LENGTH, "<img src=\"Keypad_empty.bmp\">");
+		}
+		writestring(sock, (const char *)buffer);
+	}
+}
+
+void ADHandler(int sock, PCSTR url) {
+	char buffer[MAX_COUNTER_BUFFER_LENGTH+1];
+
+	if ((sock > 0) && (url != NULL)) {
+		iprintf(url);
+		myData.Lock();
+		char *ADSegment = myData.GetADSegment();
+		myData.Unlock();
+
+		snprintf(buffer, MAX_COUNTER_BUFFER_LENGTH, "<img src=\"BarGraph%d.bmp\">", ADSegment);
+		writestring(sock, (const char *)buffer);
 	}
 }
